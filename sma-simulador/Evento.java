139c977dd45a1fb32745b46f9c2a9b5c11817e6e
Bernardo_Eckert_Wesoly <141247050+haXBlack116@users.noreply.github.com>
2025-10-01 20:50:41 -0300

    Add files via upload

---
+++ sma-simulador/Evento.java
@@ -0,0 +1,27 @@
+public class Evento implements Comparable<Evento> {
+    public static final int CHEGADA = 0;
+    public static final int SAIDA = 1;
+    public static final int PASSAGEM = 2;
+
+    private final int tipo;
+    private final double tempo;
+
+    public Evento(int tipo, double tempo) {
+        this.tipo = tipo;
+        this.tempo = tempo;
+    }
+
+    @Override
+    public int compareTo(Evento outroEvento) {
+        return Double.compare(this.tempo, outroEvento.tempo);
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public double getTempo() {
+        return tempo;
+    }
+
+}
